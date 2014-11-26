db902d44b51571e595b0b3fc84586dee939a9fb8
jammers <jammers@jeee.(none)>
2014-11-26 16:29:42 +0400

    commit

--- plotter.py
+++ plotter.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt
 
-last_digit = 0
 
+last_digits = [0,0,0,0]
 
 def plot(distribution,digit_number,current_digit):
     global last_digit
@@ -17,9 +17,12 @@ def plot(distribution,digit_number,current_digit):
     plt.ylabel('%')
     plt.barh(y_pos, x_data, align='center', alpha=0.4)
     plt.yticks(y_pos, y_data)
-    plt.text(0,10,'%d%d' % (last_digit,current_digit))
-    plt.text(0,10.4,'%09d' % digit_number)
+    for i in range(len(last_digits)-1):
+        last_digits[i] = last_digits[i+1]
+    last_digits[3] = current_digit
+    plt.text(0,10,'%d%d%d%d' % (last_digits[0],last_digits[1],last_digits[2],last_digits[3]))
+    plt.text(0,10.4,'digit: %09d' % digit_number)
 
     plt.savefig('images/%09d.png' % digit_number)
     #plt.show()
-    last_digit = current_digit
+
